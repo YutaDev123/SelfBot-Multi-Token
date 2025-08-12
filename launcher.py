89fc637a4c3cc7c00a5bac8723f40d5343b583d8
YutaDev123 <acquyfifai1301@gmail.com>
2025-08-12 21:06:14 +0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,15 +1,17 @@
 import os
 import requests
 
-URL = "https://raw.githubusercontent.com/YutaDev123/SelfBot-Multi-Token/obf-selfbotupdate.py"
-SCRIPT_FILE = "obf-selfbotupdate.py"
+URL = "https://raw.githubusercontent.com/YutaDev123/SelfBot-Multi-Token/main/main.py"
+SCRIPT_FILE = "main.py"
+WORK_DIR = "/sdcard/Download"  # Thư mục bạn muốn chạy script
 
 def update_script():
     try:
         print("[*] Đang kiểm tra cập nhật từ GitHub...")
         r = requests.get(URL)
         if r.status_code == 200:
-            with open(SCRIPT_FILE, "wb") as f:
+            # Lưu file vào WORK_DIR
+            with open(os.path.join(WORK_DIR, SCRIPT_FILE), "wb") as f:
                 f.write(r.content)
             print("[+] Đã tải bản mới nhất.")
         else:
@@ -19,6 +21,7 @@ def update_script():
 
 def run_script():
     print("[*] Đang chạy file main.py ...")
+    os.chdir(WORK_DIR)
     os.system(f"python {SCRIPT_FILE}")
 
 if __name__ == "__main__":
