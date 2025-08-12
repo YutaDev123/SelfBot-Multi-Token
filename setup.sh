e1fea646474457f98f7612f721be8c528df6ef25
YutaDev123 <acquyfifai1301@gmail.com>
2025-08-12 20:58:30 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg update -y && pkg upgrade -y
-pkg install python git -y
+pkg install python git curl -y
 pip install --upgrade pip
 pip install requests discord.py colorama aiohttp
 
-echo -e "\e[32mDone Setup!Run Tool: python launcher.py\e[0m"
+mkdir -p /sdcard/Download
+
+curl -o /sdcard/Download/launcher.py https://raw.githubusercontent.com/YutaDev123/SelfBot-Multi-Token/main/launcher.py
+
+echo -e "\e[32mDone Setup! Launcher đã được tải về "
+echo -e "Run tool bằng lệnh:\ncd /sdcard/Download && python launcher.py\e[0m"
